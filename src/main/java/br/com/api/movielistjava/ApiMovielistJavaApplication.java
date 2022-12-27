cd5491753b18edf93d56a34b87c36bc7157982bd
Wellignton <welliborba@gmail.com>
2022-12-27 11:55:43 -0300

    Criação do projeto

---
+++ src/main/java/br/com/api/movielistjava/ApiMovielistJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.movielistjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMovielistJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMovielistJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/movielistjava/ApiMovielistJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.movielistjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMovielistJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
